c509d73678ea61214b00eeb5c8d50e9c534bd2d2
Гавриличев Дмитрий <DGavrilichev@itsbel.by>
2019-09-13 17:50:50 +0300

    wip

--- ConsoleSouls/Program.cs
+++ ConsoleSouls/Program.cs
@@ -28,7 +28,7 @@ namespace ConsoleSouls
             // Any startup code for your game. We will use an example console for now
             var startingConsole = SadConsole.Global.CurrentScreen;
             startingConsole.FillWithRandomGarbage();
-            startingConsole.Fill(new Rectangle(3, 3, 27, 5), null, Color.Black, 0, SpriteEffects.None);
+            startingConsole.Fill(new Rectangle(75, 3, 27, 5), null, Color.Black, 0, SpriteEffects.None);
             startingConsole.Print(6, 5, "Hello from SadConsole", ColorAnsi.CyanBright);
         }
     }
