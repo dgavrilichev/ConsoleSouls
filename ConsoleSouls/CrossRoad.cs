36b5767b680c359fc03780b03ceb7a920ad71ad4
rudopis <an.rudopis@gmail.com>
2019-09-14 22:33:29 +0300

    wip

--- ConsoleSouls/CrossRoad.cs
+++ ConsoleSouls/CrossRoad.cs
@@ -4,9 +4,15 @@ namespace ConsoleSouls
 {
     internal sealed class CrossRoad : IDrawContent
     {
+        internal Room Room1 { get; set; }
+        internal Room Room2 { get; set; }
+        internal Room Room3 { get; set; }
+
         public void OnUpdate(GameTime gameTime)
         {
-            throw new System.NotImplementedException();
+            Room1.OnUpdate(gameTime);
+            Room2.OnUpdate(gameTime);
+            Room3.OnUpdate(gameTime);
         }
     }
 }
--- ConsoleSouls/Program.cs
+++ ConsoleSouls/Program.cs
@@ -6,10 +6,8 @@ namespace ConsoleSouls
 {
     class Program
     {
-
-
-        public const int Width = 80;
-        public const int Height = 25;
+        public const int Width = 128;
+        public const int Height = 48;
 
         static void Main(string[] args)
         {
@@ -29,8 +27,6 @@ namespace ConsoleSouls
             // Any startup code for your game. We will use an example console for now
             var startingConsole = SadConsole.Global.CurrentScreen;
             startingConsole.FillWithRandomGarbage();
-            startingConsole.Fill(new Rectangle(75, 3, 27, 5), null, Color.Black, 0, SpriteEffects.None);
-            startingConsole.Print(6, 5, "Hello from SadConsole", ColorAnsi.CyanBright);
         }
     }
 }
