d9a4b90f3be39fe9d96931ceca4adcbb66b7643d
rudopis <an.rudopis@gmail.com>
2019-09-14 18:54:50 +0300

    wip

---
+++ ConsoleSouls/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JetBrains.Annotations;
+using Microsoft.Xna.Framework;
+
+namespace ConsoleSouls
+{
+    internal abstract class Room : IDrawContent
+    {
+        [NotNull] private readonly Actor _actor;
+
+        protected Room([NotNull] Actor actor)
+        {
+            _actor = actor ?? throw new ArgumentNullException(nameof(actor));
+        }
+
+        public void OnUpdate([NotNull] GameTime gameTime)
+        {
+            _actor.OnUpdate(gameTime);
+        }
+    }
+}
