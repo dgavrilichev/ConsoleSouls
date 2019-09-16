f170b08cf0dc757a7f9aedd9d8228fafb111a3a6
rudopis <an.rudopis@gmail.com>
2019-09-17 02:23:49 +0300

    wip

--- ConsoleSouls/Skill.cs
+++ ConsoleSouls/Skill.cs
@@ -3,7 +3,8 @@
     internal sealed class Skill
     {
         internal string Name { get; }
-
         internal double CastTime { get; }
+        internal double Charge1Percent { get; }
+        internal double Charge2Percent { get; }
     }
 }
