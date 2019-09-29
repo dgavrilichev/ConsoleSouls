6be02b53a610783d9ad9046899323a258150dcef
rudopis <an.rudopis@gmail.com>
2019-09-29 23:37:50 +0300

    wip

--- ConsoleSouls/Player.cs
+++ ConsoleSouls/Player.cs
@@ -2,7 +2,9 @@
 {
     internal sealed class Player
     {
-        internal LevelValue<int> Health { get; set; } = new LevelValue<int>(10);
+        internal LevelValue<int> Health { get; set; } = new LevelValue<int>(50);
+        internal LevelValue<int> Mana { get; set; } = new LevelValue<int>(50);
+        internal LevelValue<int> Stamina { get; set; } = new LevelValue<int>(50);
         internal int Experience { get; set; }
         internal int Level { get; } = 1;
     }
