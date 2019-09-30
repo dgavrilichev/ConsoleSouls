031f2f14ca043084285878f87e754aad6c6686ba
rudopis <an.rudopis@gmail.com>
2019-09-30 08:15:35 +0300

    wip

---
+++ ConsoleSouls/ProgressBar.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using Microsoft.Xna.Framework;
+
+namespace ConsoleSouls
+{
+    internal sealed class ProgressBar : IDrawContent
+    {
+        [NotNull] private readonly LevelValue<int> _value;
+
+        internal ProgressBar([NotNull] LevelValue<int> value)
+        {
+            _value = value;
+        }
+
+        public void OnUpdate(GameTime gameTime)
+        {
+            
+        }
+    }
+}
