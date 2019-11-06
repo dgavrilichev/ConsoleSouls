82014a58c32607ef6c0f2a1d3d905299775f6129
Гавриличев Дмитрий <DGavrilichev@itsbel.by>
2019-11-06 17:30:19 +0300

    Wip

--- LineageServerCommon/ServerMain.cs
+++ LineageServerCommon/ServerMain.cs
@@ -1,12 +1,17 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
+using Common;
+using JetBrains.Annotations;
 
 namespace LineageServerCommon
 {
     public sealed class ServerMain
     {
-        public ServerMain()
-        {
+        [NotNull] private readonly ILogger _logger;
 
+        public ServerMain([NotNull] ILogger logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public async void Start(CancellationToken cancellationToken)
