eeffe26a627ea0e7bc486d0081560f4e9c312f78
Гавриличев Дмитрий <DGavrilichev@itsbel.by>
2019-11-06 17:48:05 +0300

    wip

--- LineageServerConsole/Program.cs
+++ LineageServerConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Common;
 using LineageServerCommon;
@@ -7,10 +8,23 @@ namespace LineageServerConsole
 {
     internal static class Program
     {
-        private static async Task Main()
+        private static void Main()
         {
-            var server = new ServerMain(new ThreadSafeLogger(new ConsoleLogger()));
-            await server.Start(new CancellationToken());
+            Console.CursorVisible = false;
+
+            var logger = new ThreadSafeLogger(new ConsoleLogger());
+            var server = new ServerMain(logger);
+
+            using (var cts = new CancellationTokenSource())
+            {
+                Task.Run((() => server.Start(cts.Token)), cts.Token);
+                logger.WriteInformation("Press any key to stop server..");
+                Console.ReadKey(true);
+                cts.Cancel();
+
+                logger.WriteInformation("Press any key to close..");
+                Console.ReadKey(true);
+            }
         }
     }
 }
