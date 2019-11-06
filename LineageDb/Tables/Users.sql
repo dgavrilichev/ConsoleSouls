ddf3dc23703997717c323783299c414a1759719d
rudopis <an.rudopis@gmail.com>
2019-11-06 23:51:02 +0300

    wip

--- LineageDb/Tables/Users.sql
+++ LineageDb/Tables/Users.sql
@@ -1,4 +1,10 @@
 ﻿CREATE TABLE [dbo].[Users]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL IDENTITY(1,1), 
+    [Username] NVARCHAR(50) NOT NULL, 
+    [Password] NVARCHAR(50) NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL,
+	CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
 )
+
+
