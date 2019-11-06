cad8d652ee849a62cde766e2011c491ba50ff531
Гавриличев Дмитрий <DGavrilichev@itsbel.by>
2019-11-06 17:24:46 +0300

    Wip

---
+++ Common/ConsoleLogger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Common
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Used to log messages in simulator and unit test projects.
+    /// </summary>
+    public sealed class ConsoleLogger : ILogger
+    {
+        /// <inheritdoc />
+        public void WriteError(string message)
+        {
+            WriteDateTime();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        /// <inheritdoc />
+        public void WriteWarning(string message)
+        {
+            WriteDateTime();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        /// <inheritdoc />
+        public void WriteInformation(string message)
+        {
+            WriteDateTime();
+            Console.WriteLine(message);
+        }
+
+        public void WriteDebugInformation(string message)
+        {
+            WriteDateTime();
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("[DEBUG] ");
+            Console.ResetColor();
+            Console.WriteLine(message);
+        }
+
+        private static void WriteDateTime()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write($"[{DateTime.UtcNow:HH:mm:ss.fff}] ");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Common/ILogger.cs
@@ -0,0 +1,32 @@
+﻿namespace Common
+{
+    /// <summary>
+    /// Used to write messages.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Writes error message.
+        /// Use it in case of error that prevents normal work.
+        /// </summary>
+        void WriteError(string message);
+
+        /// <summary>
+        /// Writes warning message.
+        /// Use it when system meets expected error or result
+        /// that should be noticed by administrator.
+        /// </summary>
+        void WriteWarning(string message);
+
+        /// <summary>
+        /// Writes information message.
+        /// </summary>
+        void WriteInformation(string message);
+
+        /// <summary>
+        /// Writes message related to debug.
+        /// Should not be written in system log in production.
+        /// </summary>
+        void WriteDebugInformation(string message);
+    }
+}
