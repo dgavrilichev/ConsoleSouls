60c3944b4fb153d82daeacf5ea2a95a85d9cd3b5
rudopis <an.rudopis@gmail.com>
2019-11-06 23:58:16 +0300

    Wip

--- LineageDb/Tables/Users.sql
+++ LineageDb/Tables/Users.sql
@@ -4,7 +4,8 @@
     [Username] NVARCHAR(50) NOT NULL, 
     [Password] NVARCHAR(50) NOT NULL, 
     [CreateDate] DATETIME2 NOT NULL,
-	CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+	CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [AK_Username] UNIQUE(Username)  
 )
 
 
