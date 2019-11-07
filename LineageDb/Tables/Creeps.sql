9bc13397548c7cdd933e0917cda01e35f1ffa919
rudopis <an.rudopis@gmail.com>
2019-11-07 22:48:24 +0300

    wip

--- LineageDb/Tables/Creeps.sql
+++ LineageDb/Tables/Creeps.sql
@@ -3,4 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] NVARCHAR(50) NOT NULL, 
     [Behaviour] INT NOT NULL, --1 = passive, 2 = aggressive
+	[Experience] INT NOT NULL, 
+    [Hp] INT NOT NULL, 
+
 )
