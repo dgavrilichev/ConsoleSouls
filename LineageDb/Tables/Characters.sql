b459b294ee7021ce9a475ca3086488b27658082c
rudopis <an.rudopis@gmail.com>
2019-11-07 21:15:40 +0300

    wip

--- LineageDb/Tables/Characters.sql
+++ LineageDb/Tables/Characters.sql
@@ -1,4 +1,7 @@
 ﻿CREATE TABLE [dbo].[Characters]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Experience] INT NOT NULL, 
+    [Level] INT NOT NULL
 )
