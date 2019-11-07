588035950d272a97b81e1a25cfe2baf5b4c4b8a7
rudopis <an.rudopis@gmail.com>
2019-11-07 21:28:22 +0300

    Characters table

--- LineageDb/Tables/Characters.sql
+++ LineageDb/Tables/Characters.sql
@@ -1,7 +1,11 @@
 ﻿CREATE TABLE [dbo].[Characters]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL IDENTITY(1,1), 
     [Name] NVARCHAR(50) NOT NULL, 
     [Experience] INT NOT NULL, 
-    [Level] INT NOT NULL
+    [Level] INT NOT NULL,
+	[CreateDate] DATETIME2 NOT NULL,
+	CONSTRAINT [PK_Characters] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [AK_Name] UNIQUE(Name)  
+
 )
