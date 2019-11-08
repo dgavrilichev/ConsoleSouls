8f3d5b2f1a68b13bb4569887dda5c8bab5aa719e
rudopis <an.rudopis@gmail.com>
2019-11-08 19:31:53 +0300

    wip

--- LineageDb/Tables/Items.sql
+++ LineageDb/Tables/Items.sql
@@ -1,4 +1,8 @@
 ﻿CREATE TABLE [dbo].[Items]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [ItemType] INT NOT NULL, 
+    [Price] INT NOT NULL, 
+    [Bodypart] INT NOT NULL, --1 weapon, 2 armor, 3 etcItem
 )
