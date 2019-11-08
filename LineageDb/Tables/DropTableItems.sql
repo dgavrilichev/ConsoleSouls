3f08ba6bc4ca96fb8cc86edfef33cdb1c8e13412
rudopis <an.rudopis@gmail.com>
2019-11-08 23:13:21 +0300

    wip

---
+++ LineageDb/Tables/DropTableItems.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[DropTableItems]
+(
+	[Id] INT NOT NULL IDENTITY(1,1), 
+    [DropTableId] INT NOT NULL,
+	[ItemId] INT NOT NULL,
+	[Chance] FLOAT NOT NULL, 
+    CONSTRAINT [PK_DropTableItems] PRIMARY KEY CLUSTERED ([Id] ASC)
+)
