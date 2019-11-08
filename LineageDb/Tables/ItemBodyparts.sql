26b3a914fd928654632ebe36b004801f63160614
rudopis <an.rudopis@gmail.com>
2019-11-08 19:33:42 +0300

    wip

--- LineageDb/Tables/ItemBodyparts.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[ItemBodyparts]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] NCHAR(10) NOT NULL
-)
--- LineageDb/Tables/Items.sql
+++ LineageDb/Tables/Items.sql
@@ -3,6 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] NVARCHAR(50) NOT NULL, 
     [ItemType] INT NOT NULL, 
-    [Price] INT NOT NULL, 
-    [Bodypart] INT NOT NULL, --1 weapon, 2 armor, 3 etcItem
+    [Data] NVARCHAR(MAX) NOT NULL, --1 weapon, 2 armor, 3 etcItem
+
 )
