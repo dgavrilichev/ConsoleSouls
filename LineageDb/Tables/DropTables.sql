ebc3002666d0e337e686f713e6ee4e3f4023ec0e
rudopis <an.rudopis@gmail.com>
2019-11-08 23:30:14 +0300

    Fk

--- LineageDb/Tables/DropTables.sql
+++ LineageDb/Tables/DropTables.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[DropTables]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
+	[CreepId] INT NOT NULL,
     [Chance] FLOAT NOT NULL,
-    CONSTRAINT [PK_DropTables] PRIMARY KEY CLUSTERED ([Id] ASC)
+    CONSTRAINT [PK_DropTables] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_DropTable_Creep] FOREIGN KEY ([CreepId]) REFERENCES [Creeps]([Id])
 )
